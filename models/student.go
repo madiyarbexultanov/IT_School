318bc081f90ae7fab6851762da80f2dc2d4f0bd4
MoldirBerikhanova <moldirberikhanova@gmail.com>
2025-04-04 20:43:43 +0500

    first commit

--- models/student.go
+++ models/student.go
@@ -6,11 +6,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type LessonsFilters struct {
-	PaymentStatus string
-	LessonsStatus string
-}
-
 type Student struct {
 	Id                uuid.UUID  `json:"id"`
 	FullName          string     `json:"full_name"`
@@ -22,15 +17,4 @@ type Student struct {
 	PlatformLink      string     `json:"platform_link"`
 	CrmLink           string     `json:"crm_link"`
 	CreatedAt         *time.Time `json:"created_at"`
-}
-
-type Lessons struct {
-	Id            uuid.UUID  `json:"id"`
-	StudentId     uuid.UUID  `json:"student_id"`
-	Date          *time.Time `json:"date"`
-	Feedback      string     `json:"feedback"`
-	PaymentStatus string     `json:"payment_status"`
-	LessonsStatus string     `json:"lessons_status"`
-	FeedbackDate  *time.Time `json:"feedback_date"`
-	CreatedAt     *time.Time `json:"created_at"`
 }
