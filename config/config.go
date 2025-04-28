2b2f8cbff37d82f1df16b62f07242534e883ea80
madiyarbexultanov <121680383+madiyarbexultanov@users.noreply.github.com>
2025-04-28 12:17:34 +0200

    Update config.go

--- config/config.go
+++ config/config.go
@@ -5,16 +5,15 @@ import "time"
 var Config *MapConfig
 
 type MapConfig struct {
-	AppHost            string  		 `mapstructure:"APP_HOST"`
 	DbConnectionString string  		 `mapstructure:"DATABASE_URL"`
 	JwtSecretKey       string  		 `mapstructure:"JWT_SECRET_KEY"`
-	JwtExpiresIn       time.Duration `mapstructure:"JWT_EXPIRE_DURATION"`
+	JwtExpiresIn       time.Duration 	 `mapstructure:"JWT_EXPIRE_DURATION"`
 	SMTPEmail   	   string 		 `mapstructure:"SMTP_EMAIL"`
-    SMTPPassword 	   string 		 `mapstructure:"SMTP_PASSWORD"`
-    SMTPHost     	   string 		 `mapstructure:"SMTP_HOST"`
-    SMTPPort     	   string 		 `mapstructure:"SMTP_PORT"`
-    Initial_Password   string 		 `mapstructure:"INITIAL_PASSWORD"`
-    Admin_Name   	   string 		 `mapstructure:"ADMIN_NAME"`
-    Admin_Mail   	   string 		 `mapstructure:"ADMIN_MAIL"`
-    Admin_Phone   	   string 		 `mapstructure:"ADMIN_PHONE"`
+    	SMTPPassword 	   string 		 `mapstructure:"SMTP_PASSWORD"`
+    	SMTPHost     	   string 		 `mapstructure:"SMTP_HOST"`
+    	SMTPPort     	   string 		 `mapstructure:"SMTP_PORT"`
+    	Initial_Password   string 		 `mapstructure:"INITIAL_PASSWORD"`
+   	Admin_Name   	   string 		 `mapstructure:"ADMIN_NAME"`
+    	Admin_Mail   	   string 		 `mapstructure:"ADMIN_MAIL"`
+    	Admin_Phone   	   string 		 `mapstructure:"ADMIN_PHONE"`
 }
